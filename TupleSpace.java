1e34d1304bd6569b70e32ef0d4c65d4bddbdeca6
continous-learner <3417555490@qq.com>
2025-05-03 17:07:21 +0800

    read method
    return the value of key

--- TupleSpace.java
+++ TupleSpace.java
@@ -3,4 +3,13 @@ import java.util.HashMap;
 public class TupleSpace {
 
     private final HashMap<String, String> tuples = new HashMap<>();
+
+    public synchronized String read(String key){
+        if(tuples.containsKey(key)) {
+
+        } else {
+
+        }
+    }
+
 }
