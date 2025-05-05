fce56934b7bfd0d3c2e45ec8f1aa3f019b8b7ff7
continous-learner <3417555490@qq.com>
2025-05-05 11:36:23 +0800

    Process and send the response back to the client

--- Server.java
+++ Server.java
@@ -65,6 +65,7 @@ public class Server {
                                     System.out.println("Client disconnected");
                                     break;
                                 }
+                                writer.println(processInput(request));
                             }
 
                         }
