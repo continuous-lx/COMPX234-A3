00800b669f506316eff23723dd7883638e296ae9
continous-learner <3417555490@qq.com>
2025-05-05 17:08:26 +0800

    try and catch

--- Client.java
+++ Client.java
@@ -1,12 +1,22 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
         if (args.length != 3){
             System.out.println("Error: invalid number of arguments");
             return;
         }
-        
+
         String hostname = args[0];
         int port = Integer.parseInt(args[1]);
         String filename = args[2];
+
+        try (Socket socket = new Socket(hostname, port)) {
+            
+        }
+        catch (IOException e){
+                System.out.println("Error: " + e.getMessage());
+            }
     }
 }
