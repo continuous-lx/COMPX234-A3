211d2ee5e5e6586b25ffccea3aa53c1e8b304843
continous-learner <3417555490@qq.com>
2025-05-05 17:01:36 +0800

    Makes sure of valid number of arguments

--- Client.java
+++ Client.java
@@ -1,3 +1,8 @@
 public class Client {
-    
+    public static void main(String[] args) {
+        if (args.length != 3){
+            System.out.println("Error: invalid number of arguments");
+            return;
+        }
+    }
 }
